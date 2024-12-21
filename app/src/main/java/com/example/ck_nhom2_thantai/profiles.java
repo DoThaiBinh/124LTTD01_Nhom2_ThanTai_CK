cea6b1032287df0d41b5d441416ae56d2d1f1f8d
Phamtam23 <phamthientam20041@gmail.com>
2024-12-21 08:25:19 +0700

    add profile

---
+++ app/src/main/java/com/example/ck_nhom2_thantai/profiles.java
@@ -0,0 +1,42 @@
+package com.example.ck_nhom2_thantai;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class profiles extends AppCompatActivity {
+    private ImageButton bt_load;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_profiles);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        bt_load = findViewById(R.id.Load_hoso);
+
+
+
+        //load về
+        bt_load.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(profiles.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        //Thêm giao diện đây chỉ để tạm thời
+    }
+}
